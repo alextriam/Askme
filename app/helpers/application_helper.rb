1e3e740ab8bea93d5625c48b63da5ecd8be20eab
alextriam <alextriam@gmail.com>
2020-08-16 22:07:46 +0300

    add new action code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,24 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def word_form(quantity)
+      form = {one: 'вопрос', two_four: 'вопроса', more: 'вопросов'}
+      last_number = quantity % 100
+
+      if last_number >= 10 && last_number <= 20
+        result = last_number
+      else
+        result = quantity % 10
+      end
+
+      case result
+      when 1
+        form[:one]
+      when 2..4
+        form[:two_four]
+      else
+        form[:more]
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:destroy]
   resources :questions
   root to: 'users#index'
-  # get 'users/index'
-  # get 'users/new'
-  # get 'users/edit'
-  # get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get 'show' => 'users#show'
 end
