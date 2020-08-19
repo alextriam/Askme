0a06ffbb3a02c187bbc7b151d3fc34bddfc0e83a
alextriam <alextriam@gmail.com>
2020-08-19 23:41:43 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :questions, except: [:show, :new, :index]
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: [:destroy]
   resources :questions
   root to: 'users#index'
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
