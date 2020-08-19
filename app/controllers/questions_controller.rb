1b27bc9fdf4f7295133a042648737112e0102c7b
alextriam <alextriam@gmail.com>
2020-08-19 23:41:24 +0300

    add controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,55 @@
+class QuestionsController < ApplicationController
+  before_action :load_question, only: [:edit, :update, :destroy]
+  before_action :authorize_user, except: [:create]
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      # После сохранения вопроса редиректим на пользователя
+      redirect_to user_path(@question.user), notice: 'Вопрос задан'
+    else
+      render :edit
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      # Если обновил вопрос, то тоже редиректим на страницу юзера
+      redirect_to user_path(@question.user), notice: 'Вопрос сохранен'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = @question.user
+
+    # Удаляем вопрос
+    @question.destroy
+
+    # Отправляем пользователя на страницу адресата вопроса с сообщением
+    redirect_to user_path(user), notice: 'Вопрос удален :('
+  end
+
+  private
+
+  def question_params
+    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
+    # он может менять ответы на вопрос, ему доступно и поле :answer.
+    if current_user.present? &&
+      params[:question][:user_id].to_i == current_user.id
+      params.require(:question).permit(:user_id, :text, :answer)
+    else
+      params.require(:question).permit(:user_id, :text)
+    end
+  end
+
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def authorize_user
+    reject_user unless @question.user == current_user
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,73 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+  # Порядок before_action очень важен! Они выполняются сверху вниз
+
+  # Проверяем, имеет ли юзер доступ к экшену, делаем это для всех действий, кроме
+  # :index, :new, :create, :show — к ним есть доступ у всех, даже у анонимных юзеров.
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
+  # Реализуем метод в секции private
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
   def index
-    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
-    # У каждого юзера мы прописали id, чтобы сымитировать реальную
-    # ситуацию – иначе не будет работать хелпер путей
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Vadim',
-        username: 'installero',
-        avatar_url: 'https://secure.gravatar.com/avatar/' \
-          '71269686e0f757ddb4f73614f43ae445?s=100'
-      ),
-      User.new(id: 2, name: 'Misha', username: 'aristofun')
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
     @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+      @user = User.new(user_params)
+
+      if @user.save
+        redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      else
+        render 'new'
+      end
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-    name: 'Vadim',
-    username: 'installero'
+    @user = User.find params[:id]
+    # берём вопросы у найденного юзера
+    @questions = @user.questions.order(created_at: :desc)
+    @question_form = helpers.question_form(@questions.size, form={one: 'вопрос', two_four: 'вопроса', more: 'вопросов'})
 
-    )
+    # Для формы нового вопроса создаём заготовку, вызывая build у результата вызова метода @user.questions.
+    @new_question = @user.questions.build
+  end
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'))
-    ]
+  def update
+    # Получаем параметры нового (обновленного) пользователя с помощью метода user_params
+    @user = User.find params[:id]
+    # пытаемся обновить юзера
+    if @user.update(user_params)
+      # Если получилось, отправляем пользователя на его страницу с сообщением
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      # Если не получилось, как и в create, рисуем страницу редактирования
+      # пользователя со списком ошибок
+      render 'edit'
+    end
+  end
 
-    @new_question = Question.new
+  private
 
-    @question_quantity = @questions.size
-    @question_form = helpers.word_form(@question_quantity)
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
+  end
+
+  def load_user
+    # защищаем от повторной инициализации с помощью ||=
+    @user ||= User.find params[:id]
   end
 end
