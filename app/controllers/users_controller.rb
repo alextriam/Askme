5e561e4272685de21d91a97bbc3121aa1b0f0b02
alextriam <alextriam@gmail.com>
2020-08-23 17:05:14 +0300

    add user logged after resitration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,9 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
 
       if @user.save
-        redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+        current_user = @user
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: 'вы успешно зарегистрированы, добро пожаловть на сайт'
       else
         render 'new'
       end
