8a02572f333d0db55e15466865987c28581b231c
alextriam <alextriam@gmail.com>
2020-11-07 13:09:31 +0200

    refactoring

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,7 +3,7 @@ module QuestionsHelper
     if question.author
       link_to "@#{question.author.username}", user_path(question.author)
     else
-        "<span><i>Anonimus</i></span>".html_safe
-     end
-   end
+      "<span><i>Anonimus</i></span>".html_safe
+    end
+  end
 end
